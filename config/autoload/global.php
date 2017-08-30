137e4c281bb6c588bfaa047f2efc51a9802727d6
John Meah <johnmeah@memreas.com>
2017-08-30 12:57:04 -0400

    .

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,22 +11,6 @@
  * file.
  */
 return array (
-		'db' => array (
-				'adapters' => array (
-						'memreasintdb' => array (
-								'driver' => 'Pdo',
-								'driver_options' => array (
-										PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'' 
-								) 
-						),
-						'memreasbackenddb' => array (
-								'driver' => 'Pdo',
-								'driver_options' => array (
-										PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'' 
-								) 
-						) 
-				) 
-		),
 		'service_manager' => array (
 				'abstract_factories' => array (
 						'Zend\Db\Adapter\AdapterAbstractServiceFactory' 
